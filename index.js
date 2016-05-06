7faf5651d496e45babe57452ec02dd7348edb4e2
ismamz <ismael@isma.uy>
2016-05-06 17:36:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,115 @@
+var postcss = require('postcss');
+var parser  = require('postcss-value-parser');
+
+// import functions
+var aspectRatio =       require('./lib/aspect-ratio');
+var circle =            require('./lib/circle');
+var clearfix =          require('./lib/clearfix');
+var hdBreakpoint =      require('./lib/hd-breakpoint');
+var hideVisually =      require('./lib/hide-visually');
+var horizontalRule =    require('./lib/horizontal-rule');
+var hover =             require('./lib/hover');
+var noJs =              require('./lib/no-js');
+var resetList =         require('./lib/reset-list');
+var textHide =          require('./lib/text-hide');
+var triangle =          require('./lib/triangle');
+var truncate =          require('./lib/truncate');
+var truncateMultiline = require('./lib/truncate-multiline');
+
+// list of utilities names
+var names = [
+    'aspect-ratio',
+    'circle',
+    'clearfix',
+    'hd',
+    'hide-visually',
+    'horizontal-rule',
+    'hover',
+    'no-js',
+    'hr',
+    'reset-list',
+    'text-hide',
+    'triangle',
+    'truncate'
+];
+
+// main plugin
+module.exports = postcss.plugin('postcss-utilities', function (opts) {
+    opts = opts || {};
+
+    return function (css, result) {
+        css.walkAtRules('util', function (util) {
+            // get name
+            var name = util.params.split(/\(/, 1)[0].trim();
+
+            // check if the utility is available
+            if (names.indexOf(name) <= -1) {
+                throw util.error('Unknown utility ' + name);
+            }
+
+            // save the params in a args array
+            var args = [];
+            args.push(name); // name as first argument
+            var str = parser(util.params);
+            str.nodes.forEach(function (node) {
+                if (node.type === 'function') {
+                    node.nodes.forEach(function (i) {
+                        if (i.type === 'word') {
+                            args.push(i.value);
+                        }
+                    });
+                }
+            });
+
+            // case by case
+            switch (name) {
+            case 'truncate':
+                if (args.length > 1) {
+                    truncateMultiline(util, args);
+                } else {
+                    truncate(util, args);
+                }
+                break;
+            case 'hide-visually':
+                hideVisually(util, args);
+                break;
+            case 'clearfix':
+                clearfix(util, args);
+                break;
+            case 'text-hide':
+                textHide(util);
+                break;
+            case 'aspect-ratio':
+                if (args.length > 3) {
+                    result.warn('Too many arguments for aspect-ratio utility.');
+                }
+                aspectRatio(util, args);
+                break;
+            case 'hr':
+                horizontalRule(util, args);
+                break;
+            case 'circle':
+                circle(util, args);
+                break;
+            case 'triangle':
+                triangle(util, args);
+                break;
+            case 'reset-list':
+                resetList(util, args);
+                break;
+            case 'hover':
+                hover(util, args);
+                break;
+            case 'no-js':
+                noJs(util, args);
+                break;
+            case 'hd':
+                hdBreakpoint(util, args, postcss);
+                break;
+            default:
+                break;
+            }
+        });
+
+    };
+});
---
+++ test.js
@@ -0,0 +1,58 @@
+import postcss from 'postcss';
+import test    from 'ava';
+import fs      from 'fs';
+
+import plugin from './';
+
+function run(t, input, output, opts = { }) {
+    return postcss([ plugin(opts) ]).process(input)
+        .then( result => {
+            fs.writeFileSync('test/out.css', result.css);
+            t.deepEqual(result.css, output);
+            t.deepEqual(result.warnings().length, 0);
+        });
+}
+
+test('truncate multiline', t => {
+    return run(t, 'a{ @util truncate (3, 1.5); }', 'a{ display: block; display: -webkit-box; height: 4.5em; line-height: 1.5; -webkit-line-clamp: 3; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis; }', { });
+});
+
+test('truncate multiline 2', t => {
+    return run(t, 'a{ @util truncate(3, 1.5); }', 'a{ display: block; display: -webkit-box; height: 4.5em; line-height: 1.5; -webkit-line-clamp: 3; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis; }', { });
+});
+
+test('truncate multiline 3', t => {
+    return run(t, 'a{ @util truncate(3 1.5); }', 'a{ display: block; display: -webkit-box; height: 4.5em; line-height: 1.5; -webkit-line-clamp: 3; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis; }', { });
+});
+
+test('truncate', t => {
+    return run(t, 'a{ @util truncate; }', 'a{ white-space: nowrap; overflow: hidden; text-overflow: ellipsis; }', { });
+});
+
+test('reset list', t => {
+    return run(t, 'ul{ @util reset-list; }', 'ul{ margin-top: 0; margin-bottom: 0; padding-left: 0; }\nul li{ list-style: none; }', { });
+});
+
+test('hide visually', t => {
+    return run(t, 'a{ @util hide-visually; }', 'a{ border: 0; clip: rect(0 0 0 0); height: 1px; margin: -1px; overflow: hidden; padding: 0; position: absolute; width: 1px; }', { });
+});
+
+test('clearfix', t => {
+    return run(t, 'a{ @util clearfix; }', 'a:after {\n    content: \'\';\n    display: block;\n    clear: both\n}', { });
+});
+
+test('text hide', t => {
+    return run(t, 'a{ background: #000; color: #fff; @util text-hide; }', 'a{ background: #000; color: #fff; font: 0/0 a; color: transparent; text-shadow: none; background-color: transparent; border: 0; }', { });
+});
+
+test('triangle', t => {
+    var input = fs.readFileSync('test/triangle.css', 'utf8');
+    var output = fs.readFileSync('test/triangle.expect.css', 'utf8');
+    return run(t, input, output, { });
+});
+
+test('all', t => {
+    var input = fs.readFileSync('test/test.css', 'utf8');
+    var output = fs.readFileSync('test/test.expect.css', 'utf8');
+    return run(t, input, output, { });
+});
